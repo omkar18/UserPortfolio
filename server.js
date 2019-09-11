fb4b7fbc2826bd891154d5418c0a19263276ab4d
omkar walujkar <omkarwalujkar18@gmail.com>
2019-09-11 17:43:03 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+var app = express()
+var mongooose = require("mongoose")
+var port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+const mongoURI = 'mongodb://localhost:27017/userPortfolio'
+
+mongooose.connect(mongoURI, { useNewUrlParser: true })
+    .then(() => console.log("Mongodb connected"))
+    .catch(err => console.log(err))
+
+var Users = require('./routes/Users')
+app.use('/users', Users)
+
+app.listen(port, () => {
+    console.log("server is running on port" + port)
+})
